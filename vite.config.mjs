72214ab2945cd0d556cd9d06f322710f24f27f72
Ирина Черняева <iridache@Irinas-MacBook-Air.local>
2025-02-09 22:39:27 +0300

    add base for deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/cakepan-converter',
   plugins: [
     Vue({
       template: { transformAssetUrls }
